9cdc7e66ad6790a1396e325eea098b9c2a60a853
Yoshi-k-88 <yoc1988@gmail.com>
2021-11-07 13:33:09 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-alert('Hello world');
-let favoriteFood = 'Avocado';
-document.write(favoriteFood)
+let pokemonList = [
+  {name: "Blastoise", height: 1.6, type: ["water"], ability:"Rain-dish","Torrent"},
+  {name: "Venusaur", height: 2, type: ["grass", "poison"], ability: "Chlorophyll", "Overgrow"},
+  {name: "Charizard", height: 1.7 , type: ["fire", "flying"], ability: "Blaze", "Solar-power"},
+  {name: "Beedrill", height: 1, type: ["bug","poison"], ability: "Swarm", "Sniper"},
+]
+// FOR LOOP TO DISPLAY POKEMONLIST
+let text = " ";
+for (let i = 0; i < pokemonList.length; i++){
+
+if(pokemonList[i].height <7 && pokemonList[i].height >1){
+  text = pokemonList[i];
+  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" +  "<br>" + "<br>");
