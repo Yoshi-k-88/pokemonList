12f5f91e58170eab19dfd0e41f81c4fdddbfc62e
Yoshi-k-88 <yoc1988@gmail.com>
2021-11-22 13:06:47 +1100

    Up date Java script

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,16 @@ let pokemonList = [
   {name: "Venusaur", height: 2, type: ["grass", "poison"], ability: ["Chlorophyll", "Overgrow"]},
   {name: "Charizard", height: 1.7 , type: ["fire", "flying"], ability: ["Blaze", "Solar-power"]},
   {name: "Beedrill", height: 1, type: ["bug","poison"], ability: ["Swarm", "Sniper"]},
-]
-// FOR LOOP TO DISPLAY POKEMONLIST
+  ];
+// FOR LOOP TO DISPLAY POKEMON LIST
 let text = " ";
 for (let i = 0; i < pokemonList.length; i++){
 
-if(pokemonList[i].height <7 && pokemonList[i].height >1){
+  if(pokemonList[i].height <7 && pokemonList[i].height >1)
+  {
   text = pokemonList[i];
-  console.log ('pokemonList[i]' , pokemonList[i]);
-  document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" +  "<br>" + "<br>");
+  console.log ('pokemonList[i]' ,pokemonList[i]);
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +")" +"<br>" + "<br>");
 }
 };
 document.write('<ul>');
@@ -24,5 +25,4 @@ for (let i = 0; i < pokemonList.length; i++) {
   }
   document.write('</li>');
 }
-
 document.write('</ul>');
