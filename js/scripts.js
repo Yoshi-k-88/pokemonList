4ebd024db978cc3cf51d2f107f01b3b3f71a0ec9
Yoshi-k-88 <yoc1988@gmail.com>
2021-11-22 14:51:10 +1100

    IIFE, forEach

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,23 @@ let pokemonList = [
   {name: "Charizard", height: 1.7 , type: ["fire", "flying"], ability: ["Blaze", "Solar-power"]},
   {name: "Beedrill", height: 1, type: ["bug","poison"], ability: ["Swarm", "Sniper"]},
   ];
-// FOR LOOP TO DISPLAY POKEMON LIST
-let text = " ";
-for (let i = 0; i < pokemonList.length; i++){
 
-  if(pokemonList[i].height <7 && pokemonList[i].height >1)
-  {
-  text = pokemonList[i];
-  console.log ('pokemonList[i]' ,pokemonList[i]);
-  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +")" +"<br>" + "<br>");
+let pokemonApp = (() => {
+  let getAll = () => {
+    return pokemonList
 }
-};
-document.write('<ul>');
-//Create a FOR loop to show Pokemon list
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write('<li>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
-  //Add a conditional to show big Pokemon
-  if (pokemonList[i].height >= 1) {
-    document.write(' - Wow! That\'s big!');
-  }
-  document.write('</li>');
-}
-document.write('</ul>');
+ return {
+   getAll
+ }
+})()
+
+pokemonApp.getAll().forEach((pokemon, index, pokemonList) => {
+  if(index === 1) document.write('<ul>');
+  document.write ('<li>' + pokemon.name + ' (height: ' + pokemon.height + ')');
+  //Add a conditional to show big POKEMON
+  if (pokemon.height >= 2) {
+    document.write('- Wow That\'s big!');
+    }
+    document.write('</li>');
+    if(index === pokemonList.length) document.write('</ul>');
+})
